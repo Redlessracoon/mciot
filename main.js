565cd6d21363663f01b01f82e8aaec25872161f0
Alexandr Melnic <redlessracoon@gmail.com>
2020-12-28 21:41:13 +0100

    Ball is now controlled by accel

--- main.js
+++ main.js
@@ -1,16 +1,87 @@
-window.ondevicemotion = function (event) {
-    var ax = event.accelerationIncludingGravity.x
-    var ay = event.accelerationIncludingGravity.y
-    var az = event.accelerationIncludingGravity.z
+// global vars
+var alpha = 0;
+var beta = 0;
+var gamma = 0;
+var ax = 0;
+var ay = 0;
+var az = 0;
+var agx = 0;
+var agy = 0;
+var agz = 0;
 
-    var alpha = event.rotationRate.alpha;
-    var beta = event.rotationRate.beta;
-    var gamma = event.rotationRate.gamma;
+// On pgae load
+$(function () {
+    var canvas = document.querySelector("#paddle");
+    setCanvas(canvas);
+    setupHandlers();
+    var ctx = canvas.getContext("2d");
+    var startX = canvas.width / 2;
+    var startY = canvas.height / 2;
 
-    if (alpha > 360) {
-        $("#score").text = "alpha rotation"
+    ball = new Ball(10, "#ff0000", 20, startX, startY, ctx)
+
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBall(ball);
+        updateBall(ball, -agx, agy);
     }
 
-    document.querySelector("#acc").innerHTML = "X = " + ax + "<br>" + "Y = " + ay + "<br>" + "Z = " + az;
-    document.querySelector("#acc").innerHTML += "<br>" + "alpha = " + alpha + "<br>" + "beta = " + beta + "<br>" + "gamma = " + gamma;
+    setInterval(draw, 10);
+});
+
+function Ball(radius, color, weight, x, y, ctx) {
+    this.radius = radius;
+    this.color = color;
+    this.weight = weight;
+    this.x = x;
+    this.y = y;
+    this.ctx = ctx;
+}
+
+function drawBall(ball) {
+    ball.ctx.beginPath();
+    ball.ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+    ball.ctx.fillStyle = ball.color;
+    ball.ctx.fill();
+    ball.ctx.closePath();
+}
+
+function updateBall(ball, dx, dy) {
+    ball.x += dx;
+    ball.y += dy;
+}
+
+function setupHandlers() {
+    // Check if device has motion sensors
+    if (window.DeviceMotionEvent) {
+        window.addEventListener('devicemotion', deviceMotionHandler, false);
+        document.querySelector("#support").innerHTML = "Supported!";
+    } else {
+        document.querySelector("#support").innerHTML = "Not supported!";
+    }
+}
+
+function deviceMotionHandler(event) {
+    agx = event.accelerationIncludingGravity.x;
+    agy = event.accelerationIncludingGravity.y;
+    agz = event.accelerationIncludingGravity.z;
+
+    ax = event.acceleration.x;
+    ay = event.acceleration.y;
+    az = event.acceleration.z;
+
+    alpha = event.rotationRate.alpha;
+    beta = event.rotationRate.beta;
+    gamma = event.rotationRate.gamma;
+
+    // updateBall(ball, beta, gamma)
+
+    document.querySelector("#acc").innerHTML = "GX = " + Math.round(agx) + "<br>" + "GY = " + Math.round(agy) + "<br>" + "GZ = " + Math.round(agz);
+    document.querySelector("#acc").innerHTML += "<br>" + "X = " + Math.round(ax) + "<br>" + "Y = " + Math.round(ay) + "<br>" + "Z = " + Math.round(az);
+    document.querySelector("#acc").innerHTML += "<br>" + "alpha = " + Math.round(alpha) + "<br>" + "beta = " + Math.round(beta) + "<br>" + "gamma = " + Math.round(gamma);
+}
+
+function setCanvas(canvasObject) {
+    canvasObject.width = window.innerWidth;
+    canvasObject.height = window.innerHeight;
 }
